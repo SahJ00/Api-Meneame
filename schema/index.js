0c8767eb22424b57e92e04610d5d1db7dd546a56
Sahj00 <salvador.henares@gmail.com>
2018-05-14 10:24:20 +0200

    finish

--- schema/index.js
+++ schema/index.js
@@ -9,15 +9,15 @@ type Query {
   posts: [Post]
 }
 type Mutation {
-  addPost (title: String!, img: String, nameUser: String!, date: String!, description: String!, votes: Int, category: String!):Post
+  addPost (title: String!, img: String, nameUser: String, date: String, description: String!, votes: Int, category: String!):Post
   deletePost (_id: String!):Boolean
 }
 type Post {
   _id: String
   title: String!
   img: String
-  nameUser: String!
-  date: String!
+  nameUser: String
+  date: String
   description: String!
   votes: Int
   category: String!
